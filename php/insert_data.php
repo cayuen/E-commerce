8ccd59fb4ccd201f944995629eeacae9554574b5
Christine Yuen <christineyuenn@gmail.com>
2018-05-09 02:06:19 -0700

    Item-1.php can add data to database

--- php/insert_data.php
+++ php/insert_data.php
@@ -1,4 +1,4 @@
-<?php
+<?php require_once 'connect_db.php';
 $servername = "matt-smith-v4";
 $username = "inf124db063";
 $password = "wfnt59hFDjjZ";
@@ -6,18 +6,32 @@ $dbname = "inf124db063";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if($conn === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+ 
+// Escape user inputs for security
+$full_name = mysqli_real_escape_string($conn, $_REQUEST['name']);
+$phone = mysqli_real_escape_string($conn, $_REQUEST['phone']);
+$street = mysqli_real_escape_string($conn, $_REQUEST['street']);
+$state = mysqli_real_escape_string($conn, $_REQUEST['state']);
+$zipcode = mysqli_real_escape_string($conn, $_REQUEST['zipcode']);
+$cc_no = mysqli_real_escape_string($conn, $_REQUEST['cc_no']);
+$cc_exp_month = mysqli_real_escape_string($conn, $_REQUEST['month']);
+$cc_exp_year = mysqli_real_escape_string($conn, $_REQUEST['year']);
+$cc_cvv = mysqli_real_escape_string($conn, $_REQUEST['cvv']);
 
-$conn->close();
+// attempt insert customer query execution
+$sql_customer = "INSERT INTO Customers (name, phone, street, state, zipcode, cc_no, cc_exp_month, cc_exp_year, cc_cvv)
+		VALUES ('$full_name', '$phone', '$street', '$state', '$zipcode', '$cc_no', '$cc_exp_month', '$cc_exp_year', '$cc_cvv')";
+if(mysqli_query($conn, $sql_customer)){
+    echo "Customer information added to the database!";
+} else{
+    echo "ERROR: Could not add customer to the database. $sql. " . mysqli_error($conn);
+}
+ 
+// close connection
+mysqli_close($conn);
 ?>
--- php/item-1.php
+++ php/item-1.php
@@ -6,6 +6,9 @@
 		<link rel="stylesheet" type="text/css" href="../css/text.css">
 	</head>
 
+	<script src="../js/script.js"></script>
+	<script src="../js/form.js"></script>
+	
 	<div class="background-3">	
 		<!-- .................. PHP HEADER ............... -->
 		<header>
@@ -30,52 +33,67 @@
 				"<br><br><br>" .
 				"<p>" . $row['description'] .
 				"<br>" . $row['size'] .
-				"<br>" . $row['price'] . "</p>";
+				"<br>" . $row['price'] . "</p>"
+
+
+				// attempt insert order into database
+				// $sql_order = "INSERT INTO Orders (cust_phone, product_id, quantity, shipping)
+				// 		VALUES (818914, 55, 4, "$row['price']")";
+				// if(mysqli_query($conn, $sql_order)){
+				//     echo "Customer records added successfully.";
+				// } else{
+				//     echo "ERROR: Could not to execute order query. $sql. " . mysqli_error($conn);
+				// }
+				;
 		}
 		mysqli_close($con);
+
 		?>
 
-								<!-- Drop down for size -->
 
-								<!-- Drop down for quantity -->
-								
-								<!-- Order form -->
-								<form name="order-form" action='#'>
-								    <br><strong>Purchase Method</strong><br>
-								    <label for="firstname">* First Name</label>
-								    <input type="text" name="firstname" required><br>
-								    <label for="lastname">* Last Name</label>
-								    <input type="text" name="lastname" required><br>
-								    <label for="phone">* Phone Number</label>
-								    <input type="text" name="phone" required><br>
-								    <label for="address">* Shipping Address</label>
-								    <input type="text" name="address" required><br>
-								    <label for="creditcard">* Credit Card</label>
-								    <input type="text" name="creditcard" required><br>
+		<form action="insert_data.php" method="post">
+		    <p>
+		        <label for="firstName">Full Name:</label>
+		        <input type="text" name="name" id="fullName">
+		    </p>
+		    <p>
+		        <label for="phone">Phone Number:</label>
+		        <input type="text" name="phone" id="phoneNumber">
+		    </p>
+		    <p>
+		        <label for="street">Street Address:</label>
+		        <input type="text" name="street" id="streetAddress">
+		    </p>
+		    <p>
+		        <label for="state">State:</label>
+		        <input type="text" name="state" id="state">
+		    </p>
+		    <p>
+		        <label for="zipcode">Zip Code:</label>
+		        <input type="text" name="zipcode" id="zipcode">
+		    </p>
+		    <p>
+		        <label for="cc_no">Credit Card Number:</label>
+		        <input type="text" name="cc_no" id="cc_no">
+		    </p>
+		    <p>
+		        <label for="cc_exp_month">Expiration Month:</label>
+		        <input type="text" name="month" id="month">
+		    </p>
+		    <p>
+		        <label for="cc_exp_year">Expiration Year:</label>
+		        <input type="text" name="year" id="year">
+		    </p>
+		    <p>
+		        <label for="cc_cvv">CVV:</label>
+		        <input type="text" name="cvv" id="cvv">
+		    </p>
+		    <input type="submit" value="Submit">
 
-								    <!-- Multiple choice for shipping options -->
-								    <br><strong>Shipping Method</strong><br>
-									<div>
-								        <input type="radio" name="x" id="y" value="A" />
-								        <label for="y">Overnight </label>
-								    </div>		    
-								    <div>
-								        <input type="radio" name="x" id="y" value="B" />
-								        <label for="y">2-days Expedited</label>
-								    </div>
-								    <div>
-								        <input type="radio" name="x" id="y" value="C" />
-								        <label for="y">6-days Ground</label>
-								    </div><br>      
-									
-								    <div class="submit-btn">
-								    	<input type="submit" name="submit" value="Submit" onclick="checkCreditCard(document.order-form.creditcard)"/>
-								    </div>
-								</form>
-							</td>
-						</tr>
-					</table>
-				<section>
-			</div>
-			<script src="../js/script.js"></script>
-		</html>
+		</form>
+						</td>
+					</tr>
+				</table>
+			<section>
+		</div>
+	</html>
